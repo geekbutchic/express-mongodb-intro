91bc30bcbb9b042d1245daca0000e53f3f6e8f3c
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-05-28 19:54:28 -0400

    update

--- routes/users/controller/userController.js
+++ routes/users/controller/userController.js
@@ -1,60 +1,56 @@
-// USER IS COMING FROM MONGODB SCHEMA
 const User = require("../model/User");
-var bcrypt = require('bcryptjs');
 
-// EXPORTING AN OBJECT WITH KEY AND VALUE
+const bcrypt = require("bcryptjs");
+
+async function getAllUsers(req, res) {
+  try {
+    let foundAllUsers = await User.find({});
+    res.json({ message: "success", data: foundAllUsers });
+  } catch (e) {
+    res.status(500).json({ message: "failure", error: e.message });
+  }
+}
+async function createUser(req, res) {
+  const { password, firstName, lastName, email, username } = req.body;
+  try {
+    let createdSalt = await bcrypt.genSalt(10);
+    let hashedPassword = await bcrypt.hash(password, createdSalt);
+    let newUser = new User({
+      firstName,
+      lastName,
+      email,
+      username,
+      password: hashedPassword,
+    });
+    let savedUser = await newUser.save();
+    res.json({ message: "success", data: savedUser });
+  } catch (e) {
+    res.status(500).json({ message: "failure", error: e.message });
+  }
+}
+async function updateUserByID(req, res) {
+  const id = req.params.id;
+  try {
+    let updatedUser = await User.findByIdAndUpdate({ _id: id }, req.body, {
+      new: true,
+    });
+    res.json({ message: "success", data: updatedUser });
+  } catch (e) {
+    res.status(500).json({ message: "failure", error: e.message });
+  }
+}
+async function deleteUserByID(req, res) {
+  const id = req.params.id;
+  try {
+    let deletedUser = await User.findByIdAndRemove({ _id: id });
+    res.json({ message: "success", data: deletedUser });
+  } catch (e) {
+    res.status(500).json({ message: "failure", error: e.message });
+  }
+}
 module.exports = {
-  // USER.FIND({}) IS A MONGOOSE FUNCTION TO QUERY THE DATABASE
-  // IT TAKES IN A CALLBACK - THAT RETURN TWO PARAMETERS
-  // FIRST - IS ALWAYS ERROR
-  // SECOND PAYLOAD - USER DATA
-  getAllUsers: function (callback) {
-    User.find({}, function (err, payload) {
-      if (err) {
-        callback(err, null); // CALLBACKS SAME FUNCTION
-      } else {
-        callback(null, payload);
-      }
-    });
-  },
-  createUser: function (body, callback) {
-    let createdUser = new User({
-      firstName: body.firstName,
-      lastName: body.lastName,
-      password: body.password,
-      email: body.email,
-      username: body.username,
-    });
-    createdUser.save(function (err, payload) {
-      // SAVES TO DATABASE
-      if (err) {
-        callback(err, null);
-      } else {
-        callback(null, payload);
-      }
-    });
-  },
-  updateUserByID: function (id, body, callback) {
-    User.findByIdAndUpdate(
-      { _id: id }, // MONGODB SPECIFIC _id
-      body,
-      { new: true },
-      function (err, updatedPayload) {
-        if (err) {
-          callback(err, null);
-        } else {
-          callback(null, updatedPayload);
-        }
-      }
-    );
-  },
-  deleteUserByID: function (id, callback) {
-    User.findByIdAndRemove({ _id: id }, function (err, deletedPayload) {
-      if (err) {
-        callback(err, null);
-      } else {
-        callback(null, deletedPayload);
-      }
-    });
-  },
+  getAllUsers,
+  createUser,
+  updateUserByID,
+  deleteUserByID,
 };
